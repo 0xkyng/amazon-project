0c68eb341a93cef65e8286c934d78f52348fabb5
0xkyng <teryima127@gmail.com>
2024-07-19 09:44:21 +0100

    Save data and generate HTML for delivery options

---
+++ data/deliveryOptions.js
@@ -0,0 +1,13 @@
+export const deliveryOptions = [{
+    id: '1',
+    deliveryTime: 7,
+    pricecents: 0
+},{
+    id: '3',
+    deliveryTime: 3,
+    pricecents: 499
+}, {
+    id: '3',
+    deliveryTime: 1,
+    pricecents: 999
+}];
